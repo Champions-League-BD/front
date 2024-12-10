7a8d13e8eab715e6fa4ea7e31b820088c7adedb2
gustavoborinn <gustavo.nascimento@sou.unifal-mg.edu.br>
2024-12-10 03:11:58 -0300

    fix: css style fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importa o CSS do Bootstrap
 import "./index.css";
 import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css"; // Importa o CSS do Bootstrap
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
